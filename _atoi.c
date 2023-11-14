e898af493f7e1eacd32d4b7211caea0489762864
“Akanni23“ <muhasy2k@yahoo.com>
2023-11-14 15:39:27 -0800

    atoi.c

---
+++ _atoi.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+ * interactive - shell interactive mode if returned.
+ * @info: address structure
+ *
+ * Return: 1 if interactive mode, 0 if otherwise.
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter.
+ * @c: character to be checked.
+ * @delim: delimeter strings.
+ *
+ * Return: 1 if yes, 0 if null
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha – it checks for alpha chars.
+ * @c: character to be input in charaters.
+ *
+ * Return: 1 if c is alphabetic, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * Parameters: a - input parameter for convertion of string to an interger
+ * _atoi – to converts strings.
+ * @s: strings to be converted
+ *
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
